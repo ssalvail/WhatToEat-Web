1d42a8fb2ea095d944a50ce3dc4ce5e17ed6de92
Sam Salvail <ssalvail39@gmail.com>
2018-11-08 15:58:52 -0800

    initial commit

--- src/App.js
+++ src/App.js
@@ -3,10 +3,49 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      content: "default"
+    };
+
+    // bind this
+    this.suggest = this.suggest.bind(this);
+    this.findRestaurant = this.findRestaurant.bind(this);
+    this.findBar = this.findBar.bind(this);
+  }
+
+  suggest() {
+    this.setState(state => ({
+      content: "suggest"
+    }));
+  }
+
+  findRestaurant() {
+    this.setState(state => ({
+      content: "restaurant"
+    }));
+  }
+
+  findBar() {
+    this.setState(state => ({
+      content: "bar"
+    }));
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
+          <h1 className="App-title">What To Eat</h1>
+          <button className="button-header" onClick={this.suggest}>Suggest</button>
+        </header>
+        {this.state.content}
+        <div className="App-content">
+          <button className="button-primary" onClick={this.findRestaurant}>Find a Restaurant</button>
+          <button className="button-primary" onClick={this.findBar}>Find a Bar</button>
+        </div>
+        {/* <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
             Edit <code>src/App.js</code> and save to reload.
@@ -19,7 +58,7 @@ class App extends Component {
           >
             Learn React
           </a>
-        </header>
+        </header> */}
       </div>
     );
   }
